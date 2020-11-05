2a5d7060bec8ac32f345942e8f478210101b4f99
Gabriel George <biiel.gg22@gmail.com>
2020-11-05 16:40:54 -0300

    Commit inicial, já incluindo telas de Login e Cadastro, conexões com o Firebase e modelo inicial de Usuário

---
+++ app/src/main/java/com/example/instagram/helper/ConfiguracaoFirebase.java
@@ -0,0 +1,26 @@
+package com.example.instagram.helper;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ConfiguracaoFirebase {
+
+    private static DatabaseReference referenciaFirebase;
+    private static FirebaseAuth referenciaAutenticacao;
+
+    public static DatabaseReference getFirebase(){
+        if (referenciaFirebase == null){
+            referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+        return referenciaFirebase;
+    }
+
+    //retorna a instancia do Firebase
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if( referenciaAutenticacao == null){
+            referenciaAutenticacao = FirebaseAuth.getInstance();
+        }
+        return referenciaAutenticacao;
+    }
+}
