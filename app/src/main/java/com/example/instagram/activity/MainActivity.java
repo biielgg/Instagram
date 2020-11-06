ad9ee918fc32e4076a4569b738e705ad6d2c1c0b
Gabriel George <biiel.gg22@gmail.com>
2020-11-06 17:27:35 -0300

    Toolbar inserida na main e modificada para apresentação do nome do app

--- app/src/main/java/com/example/instagram/activity/MainActivity.java
+++ app/src/main/java/com/example/instagram/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.instagram.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Configura a toolbar
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Instagram");
+        setSupportActionBar(toolbar);
     }
 }
