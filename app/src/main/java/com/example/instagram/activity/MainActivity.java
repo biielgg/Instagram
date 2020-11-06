c0c0d624444c62423d0d843dace2be3efe3b99ca
Gabriel George <biiel.gg22@gmail.com>
2020-11-06 17:44:36 -0300

    Adição do Menu lateral

--- app/src/main/java/com/example/instagram/activity/MainActivity.java
+++ app/src/main/java/com/example/instagram/activity/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.instagram.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.instagram.R;
 
@@ -20,4 +24,23 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("Instagram");
         setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch ((item.getItemId())){
+            case R.id.menu_sair:
+                //deslogarUsuario();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
